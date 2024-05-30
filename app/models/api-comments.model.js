34b0d4c38a55a68fc0c675218be386d67a378b27
samiracaruso <samiragcaruso@gmail.com>
2024-05-30 12:26:57 +0100

    Solves kata 9 - DELETE /api/comments/comment_id

--- app/models/api-comments.model.js
+++ app/models/api-comments.model.js
@@ -40,3 +40,23 @@ RETURNING *;`, [username, body, article_id])
 })
 }
 
+exports.removeComment = (comment_id) => {
+    return db.query(`SELECT * FROM comments
+    WHERE comment_id = $1`, [comment_id])
+    .then((response) => {
+        if (response.rows.length === 0){
+            return Promise.reject({status: 404, message: 'No comment found'})
+        } else {
+            return db.query(`DELETE FROM comments
+            WHERE comment_id = $1
+            RETURNING *`, [comment_id])
+        }
+    })
+    // .then(() => {
+    //     return db.query(`SELECT * FROM comments
+    //     WHERE comment_id = $1`, [comment_id])
+    // })
+    // .then((response) => {
+    //     console.log(response.rows)
+    // })
+    }
