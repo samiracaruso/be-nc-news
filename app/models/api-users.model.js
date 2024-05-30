05c39be1b1bf2472c8966949c549480b3097177b
samiracaruso <samiragcaruso@gmail.com>
2024-05-30 14:30:02 +0100

    Solves kata 10 - GET /api/users

--- app/models/api-users.model.js
+++ app/models/api-users.model.js
@@ -3,7 +3,6 @@ const db = require('../../db/connection')
 exports.selectUsers = () => {
     return db.query(`SELECT * FROM users`)
     .then((response) => {
-        console.log(response.rows)
         return response.rows
     })
 }
