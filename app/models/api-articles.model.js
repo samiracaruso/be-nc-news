364635782710f4da4167b9183e218259c5a622a2
samiracaruso <samiragcaruso@gmail.com>
2024-05-31 10:42:19 +0100

    Solves kata 12 - adds comment count to :article_id

--- app/models/api-articles.model.js
+++ app/models/api-articles.model.js
@@ -3,7 +3,20 @@ const { doesTopicExist } = require("./api-topics.model");
 
 exports.selectArticleById = (article_id) => {
   return db
-    .query("SELECT * FROM articles where article_id = $1;", [article_id])
+    .query(`SELECT 
+    a.article_id,
+    a.title,
+    a.body,
+    a.votes,
+    a.topic,
+    a.author,
+    a.created_at,
+    a.article_img_url,
+    COUNT(c.comment_id) AS comment_count
+  FROM articles a
+  LEFT JOIN comments c ON a.article_id = c.article_id
+  WHERE a.article_id = $1
+  GROUP BY a.article_id;`, [article_id])
     .then((response) => {
       if (response.rows.length === 0) {
         return Promise.reject({ status: 404, message: "Page Not Found" });
